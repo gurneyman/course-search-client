2de600457774d952f423c0f47f85734946df2074
Tom Prince <tprince14@gmail.com>
2018-01-30 19:49:04 -0500

    initial config and some state

---
+++ src/index-reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
+import client from './client/reducer'
+import signup from './signup/reducer'
+
+const IndexReducer = combineReducers({
+    signup,
+    client,
+    form,
+})
+
+export default IndexReducer
---
+++ src/signup/reducer.js
@@ -0,0 +1,24 @@
+import { SIGNUP_REQUESTING } from './constants'
+
+const initialState = {
+    requesting: false,
+    successful: false,
+    messages: [],
+    errors: [],
+}
+
+const reducer = function signupReducer (state = initialState, action) {
+    switch (action.type) {
+        case SIGNUP_REQUESTING:
+            return {
+                requesting: true,
+                successful: false,
+                messages: [{ body: 'Signing up...', time: new Date() }],
+                errors: [],
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
